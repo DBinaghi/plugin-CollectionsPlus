36eca7bf3564ea915592d5c7951fa2e50376895d
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2023-02-10 00:01:41 +0100

    Added extra option, removed empty spaces

--- views/helpers/CollectionSearchFilters.php
+++ views/helpers/CollectionSearchFilters.php
@@ -13,69 +13,70 @@
  */
 class Omeka_View_Helper_CollectionSearchFilters extends Zend_View_Helper_Abstract
 {
-    /**
-     * Get a list of the currently-active filters for collection browse/search.
-     *
-     * @param array $params Optional array of key-value pairs to use instead of
-     *  reading the current params from the request.
-     * @return string HTML output
-     */
-    public function collectionSearchFilters(array $params = null)
-    {
+	/**
+	 * Get a list of the currently-active filters for collection browse/search.
+	 *
+	 * @param array $params Optional array of key-value pairs to use instead of
+	 *  reading the current params from the request.
+	 * @return string HTML output
+	 */
+	public function collectionSearchFilters(array $params = null)
+	{
 		if ($params === null) {
-            $request = Zend_Controller_Front::getInstance()->getRequest();
-            $requestArray = $request->getParams();
-        } else {
-            $requestArray = $params;
-        }
+			$request = Zend_Controller_Front::getInstance()->getRequest();
+			$requestArray = $request->getParams();
+		} else {
+			$requestArray = $params;
+		}
 
-        $db = get_db();
-        $displayArray = array();
-        foreach ($requestArray as $key => $value) {
-            if ($value != null) {
-                $filter = ucfirst($key);
-                $displayValue = null;
-                switch ($key) {
-                    case 'user':
-                        $user = $db->getTable('User')->find($value);
-                        if ($user) {
-                            $displayValue = $user->name;
-                        }
-                        break;
-                    case 'public':
-                    case 'featured':
-                        $displayValue = ($value == 1 ? __('Yes') : $displayValue = __('No'));
-                        break;
-                    case 'search':
-                    case 'tags':
-                    case 'range':
-                        $displayValue = $value;
-                        break;
-                }
-                if ($displayValue) {
-                    $displayArray[$filter] = $displayValue;
-                }
-            }
-        }
+		$db = get_db();
+		$displayArray = array();
+		foreach ($requestArray as $key => $value) {
+			if ($value != null) {
+				$filter = ucfirst($key);
+				$displayValue = null;
+				switch ($key) {
+					case 'user':
+						$user = $db->getTable('User')->find($value);
+						if ($user) {
+							$displayValue = $user->name;
+						}
+						break;
+					case 'empty':
+					case 'public':
+					case 'featured':
+						$displayValue = ($value == 1 ? __('Yes') : $displayValue = __('No'));
+						break;
+					case 'search':
+					case 'tags':
+					case 'range':
+						$displayValue = $value;
+						break;
+				}
+				if ($displayValue) {
+					$displayArray[$filter] = $displayValue;
+				}
+			}
+		}
 
-        $displayArray = apply_filters('collection_search_filters', $displayArray, array('request_array' => $requestArray));
+		$displayArray = apply_filters('collection_search_filters', $displayArray, array('request_array' => $requestArray));
 
-        $html = '';
-        if (!empty($displayArray) || !empty($advancedArray)) {
-            $html .= '<div id="item-filters">';
-            $html .= '<ul>';
-            foreach ($displayArray as $name => $query) {
-                $class = html_escape(strtolower(str_replace(' ', '-', $name)));
-                $html .= '<li class="' . $class . '">' . html_escape(__($name)) . ': ' . html_escape($query) . '</li>';
-            }
-            if (!empty($advancedArray)) {
-                foreach ($advancedArray as $j => $advanced) {
-                    $html .= '<li class="advanced">' . html_escape($advanced) . '</li>';
-                }
-            }
-            $html .= '</ul>';
-            $html .= '</div>';
-        }
-        return $html;
-    }
+		$html = '';
+		if (!empty($displayArray) || !empty($advancedArray)) {
+			$html .= '<div id="item-filters">';
+			$html .= '<ul>';
+			foreach ($displayArray as $name => $query) {
+				$class = html_escape(strtolower(str_replace(' ', '-', $name)));
+				$html .= '<li class="' . $class . '">' . html_escape(__($name)) . ': ' . html_escape($query) . '</li>';
+			}
+			if (!empty($advancedArray)) {
+				foreach ($advancedArray as $j => $advanced) {
+					$html .= '<li class="advanced">' . html_escape($advanced) . '</li>';
+				}
+			}
+			$html .= '</ul>';
+			$html .= '</div>';
+		}
+		return $html;
+	}
 }
